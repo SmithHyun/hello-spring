6621ee31e36ad1d37aa840aa3184509a043a1a1f
Smith Hyun <jshyun91@gmail.com>
2022-09-06 14:24:17 +0900

    Third commit

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,10 +1,18 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService;
+    private final MemberService memberService;
 
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    //    @Autowired private MemberService memberService;   // 필드 주입 방법 - 권장하지 않음.
 }
+
---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//생성자 주입 3가지 방법 있음.
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
