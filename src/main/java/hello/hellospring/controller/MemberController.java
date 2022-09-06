655facb4d9e0961e4c1dc3538d26bced7d060719
joonsup <jshyun91@gmail.com>
2022-09-06 22:42:59 +0900

    feat: add memberForm

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -14,5 +15,10 @@ public class MemberController {
     }
 
     //    @Autowired private MemberService memberService;   // 필드 주입 방법 - 권장하지 않음.
+
+    @GetMapping("/members/new")
+    public String createForm() {
+            return "members/createMemberForm";
+    }
 }
 
